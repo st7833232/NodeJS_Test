934ecd206bb0448dd1f9c1832216ce2e2532a26e
Jeff <st7833232@gmail.com>
2020-03-10 17:31:27 +0800

    Update finish class

---
+++ lab11_scope/demo1.js
@@ -0,0 +1,10 @@
+console.log(x2)
+//console.log(x1)
+x1 = 'hello'
+var x2 = 'world'
+console.log(typeof x1, typeof x2)
+console.log(x1, x2)
+function printMessage() {
+    console.log( 'inside printMessage',x1, x2)
+}
+printMessage()
